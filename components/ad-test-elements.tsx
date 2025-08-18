a98c580767681330c916d531662495b1f68c240b
Mik3xTreezy <jisxhnu@gmail.com>
2025-08-18 21:10:15 +0530

    Fresh

---
+++ components/ad-test-elements.tsx
@@ -0,0 +1,106 @@
+"use client"
+
+import { useEffect } from 'react'
+
+export default function AdTestElements() {
+  useEffect(() => {
+    // Create test ad elements that ad blockers typically block
+    const testElements = [
+      {
+        id: 'adsbox',
+        className: 'adsbox',
+        style: 'position:absolute;left:-9999px;top:-9999px;width:1px;height:1px;overflow:hidden;',
+        content: '&nbsp;'
+      },
+      {
+        id: 'advertisement',
+        className: 'advertisement',
+        style: 'position:absolute;left:-9999px;top:-9999px;width:1px;height:1px;overflow:hidden;',
+        content: '&nbsp;'
+      },
+      {
+        id: 'ads',
+        className: 'ads',
+        style: 'position:absolute;left:-9999px;top:-9999px;width:1px;height:1px;overflow:hidden;',
+        content: '&nbsp;'
+      },
+      {
+        id: 'ad',
+        className: 'ad',
+        style: 'position:absolute;left:-9999px;top:-9999px;width:1px;height:1px;overflow:hidden;',
+        content: '&nbsp;'
+      },
+      {
+        id: 'adsbygoogle',
+        className: 'adsbygoogle',
+        style: 'position:absolute;left:-9999px;top:-9999px;width:1px;height:1px;overflow:hidden;',
+        content: '&nbsp;'
+      },
+      {
+        id: 'google-ad',
+        className: 'google-ad',
+        style: 'position:absolute;left:-9999px;top:-9999px;width:1px;height:1px;overflow:hidden;',
+        content: '&nbsp;'
+      },
+      {
+        id: 'doubleclick',
+        className: 'doubleclick',
+        style: 'position:absolute;left:-9999px;top:-9999px;width:1px;height:1px;overflow:hidden;',
+        content: '&nbsp;'
+      },
+      {
+        id: 'banner-ad',
+        className: 'banner-ad',
+        style: 'position:absolute;left:-9999px;top:-9999px;width:1px;height:1px;overflow:hidden;',
+        content: '&nbsp;'
+      }
+    ]
+
+    // Add test elements to the page
+    testElements.forEach(element => {
+      const div = document.createElement('div')
+      div.id = element.id
+      div.className = element.className
+      div.style.cssText = element.style
+      div.innerHTML = element.content
+      document.body.appendChild(div)
+    })
+
+    // Add test scripts that ad blockers typically block
+    const testScripts = [
+      'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+      'https://www.googletagmanager.com/gtag/js',
+      'https://www.google-analytics.com/analytics.js',
+      'https://www.googleadservices.com/pagead/conversion.js'
+    ]
+
+    testScripts.forEach(src => {
+      const script = document.createElement('script')
+      script.src = src
+      script.async = true
+      script.style.display = 'none'
+      document.head.appendChild(script)
+    })
+
+    // Cleanup function
+    return () => {
+      // Remove test elements
+      testElements.forEach(element => {
+        const el = document.getElementById(element.id)
+        if (el) {
+          el.remove()
+        }
+      })
+
+      // Remove test scripts
+      testScripts.forEach(src => {
+        const script = document.querySelector(`script[src="${src}"]`)
+        if (script) {
+          script.remove()
+        }
+      })
+    }
+  }, [])
+
+  return null
+}
--- components/anti-adblock-wrapper.tsx
+++ components/anti-adblock-wrapper.tsx
@@ -1,18 +1,23 @@
 "use client"
 
-import AntiAdblockAdvanced from './anti-adblock-advanced'
+import AntiAdblockDetector from './anti-adblock-detector'
+import AdTestElements from './ad-test-elements'
 
 export default function AntiAdblockWrapper() {
   return (
-    <AntiAdblockAdvanced 
-      onAdblockDetected={() => {
-        console.log('Ad blocker detected - user experience may be affected')
-      }}
-      onAdblockNotDetected={() => {
-        console.log('No ad blocker detected - full functionality available')
-      }}
-      showWarning={true}
-      customWarningMessage="We've detected that you're using an ad blocker. Our paste sharing service relies on advertising to remain free and accessible to everyone. Please disable your ad blocker for this site to continue using our services."
-    />
+    <>
+      <AdTestElements />
+      <AntiAdblockDetector 
+        onAdblockDetected={() => {
+          console.log('Ad blocker detected using @scthakuri/adblock-detector - user experience may be affected')
+        }}
+        onAdblockNotDetected={() => {
+          console.log('No ad blocker detected using @scthakuri/adblock-detector - full functionality available')
+        }}
+        showWarning={true}
+        checkInterval={3000}
+        customWarningMessage="We've detected that you're using an ad blocker. Our paste sharing service relies on advertising to remain free and accessible to everyone. Please disable your ad blocker for this site to continue using our services."
+      />
+    </>
   )
 }
